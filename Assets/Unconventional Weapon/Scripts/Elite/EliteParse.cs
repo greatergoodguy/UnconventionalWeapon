c439a038066783a9b2bd0f2ae0ffab84d71e4917
Tom Yu <tom.yu@burstingbrains.com>
2015-05-08 07:53:10 -0500

    Added TK2D toolkit

---
+++ Assets/Unconventional Weapon/Scripts/Elite/EliteParse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Parse;
+
+public class EliteParse : MonoBehaviour {
+
+	void Awake() {
+	}
+
+	void Start() {
+//		ParseObject testObject = new ParseObject("TestObject");
+//		testObject["foo"] = "bar";
+//		testObject.SaveAsync();
+	}
+	
+	void Update() {
+	
+	}
+}
